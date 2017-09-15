8e96879a57801b9d2e90bee5fcbeade59dcafadc
ehsan <qasemi.ehs@gmail.com>
2017-09-15 10:04:02 -0500

    adding the mkdir script

--- HWs/HW1/creat_folders.sh
+++ HWs/HW1/creat_folders.sh
@@ -1,7 +1,15 @@
-mkdir /home/ubuntu/conf, logs, software, storage, workload 
-mkdir /home/ubuntu/logs/apps, hadoop 
-mkdir /home/ubuntu/storage/data/localm, tmp 
-mkdir /home/ubuntu/storage/hdfs/hdfs__dirs, hdfs_nn_dir 
+mkdir /home/ubuntu/conf
+mkdir /home/ubuntu/logs
+mkdir /home/ubuntu/software
+mkdir /home/ubuntu/storage
+mkdir /home/ubuntu/workload
+
+mkdir /home/ubuntu/logs/apps 
+mkdir /home/ubuntu/logs/hadoop 
+mkdir /home/ubuntu/storage/data/localm
+mkdir /home/ubuntu/storage/data/tmp 
+mkdir /home/ubuntu/storage/hdfs/hdfs__dirs 
+mkdir /home/ubuntu/storage/hdfs/hdfs_nn_dir 
 mkdir /home/ubuntu/workload 
 mkdir /home/ubuntu/storage/da/spark/rdds_shuffle 
 mkdir /home/ubuntu/logs/spark 
