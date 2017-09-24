b070a671cb2ae819d5192056472f98b6920a9b03
ehsan <qasemi.ehs@gmail.com>
2017-09-24 16:00:58 -0500

    creating the MR folder for keeping the MR code

---
+++ HWs/HW1/Software_deployment.sh
@@ -0,0 +1,4 @@
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/conf.tar.gz
+tar zxvf conf.tar.gz
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/run.sh
+echo -e "10.254.0.212\n10.254.0.188\n10.254.1.6\n10.254.0.190\n10.254.0.191" >> instances
