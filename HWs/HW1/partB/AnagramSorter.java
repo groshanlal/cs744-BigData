27d5f7fdb97d893c633bbad6077fc2fea6ef3009
ehsan <qasemi.ehs@gmail.com>
2017-09-25 14:10:53 -0500

    adding the second MR. just the code

--- HWs/HW1/partB/AnagramSorter.java
+++ HWs/HW1/partB/AnagramSorter.java
@@ -15,44 +15,70 @@ public class AnagramSorter {
   
 
   public static class Anagram {
-  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-    private Text sorted = new Text();
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+      private final static IntWritable one = new IntWritable(1);
+      private Text word = new Text();
+      private Text sorted = new Text();
 
-    public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-      String line = value.toString();
-      StringTokenizer tokenizer = new StringTokenizer(line);
-      while (tokenizer.hasMoreTokens()) {
-        word.set(tokenizer.nextToken());
-        char[] chars = word.toString().toCharArray();
-        Arrays.sort(chars);
-        sorted.set(new String(chars));
-        output.collect(sorted, word);
+      public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+          word.set(tokenizer.nextToken());
+          char[] chars = word.toString().toCharArray();
+          Arrays.sort(chars);
+          sorted.set(new String(chars));
+          output.collect(sorted, word);
+        }
       }
     }
+    
+    public static class Reduce  extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+      public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        //int sum = 0;
+        //while (values.hasNext()) {
+        //  sum += values.next().get();
+        //}
+        //output.collect(key, new IntWritable(sum));
+        //ArrayList<IntWritable> list = new ArrayList<IntWritable>();    
+        StringBuilder strVal = new StringBuilder();
+        while ( values.hasNext()) {
+           
+          strVal.append(values.next().toString());
+          strVal.append(",");
+        }
+        //output.collect(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()]) ) );
+        output.collect(key, new Text(strVal.toString()));
+        //context.write(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()])));
+      }
+    }
+  }
+
+  public static class Sorter {
+    public static class Map extends MapReduceBase implements Mapper<Text, Text, IntWritable, Text> {
+      //private final static IntWritable one = new IntWritable(1);
+      private Text word = new Text(); 
+      //private Text sorted = new Text(); 
+
+      public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+          word.set(tokenizer.nextToken());
+          
+          int count = StringUtils.countMatches(word.toString(), ",");        
+          output.collect(new IntWritable(count), word);
+        }
+      }
+    }
+
+    public static class Reduce extends MapReduceBase implements Reducer<IntWritable, Text, NullWritable, Text> {
+      public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        output.collect(NullWritable.get(), values);
+      }
+    } 
   }
   
-  public static class Reduce  extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-    public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-      //int sum = 0;
-      //while (values.hasNext()) {
-      //  sum += values.next().get();
-      //}
-      //output.collect(key, new IntWritable(sum));
-      //ArrayList<IntWritable> list = new ArrayList<IntWritable>();    
-      StringBuilder strVal = new StringBuilder();
-      while ( values.hasNext()) {
-         
-        strVal.append(values.next().toString());
-        strVal.append(",");
-      }
-      //output.collect(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()]) ) );
-      output.collect(key, new Text(strVal.toString()));
-      //context.write(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()])));
-    }
-  }
-  }
   public static void main(String[] args) throws Exception {
     //Configuration conf = getConf();
     //Job job = new Job(conf, "AnagramSorter");
