f8f35b4c0c82cd761e8210b76029ff0b47392242
ehsan <qasemi.ehs@gmail.com>
2017-09-25 13:13:57 -0500

    correcting the identation

--- HWs/HW1/partB/AnagramSorter.java
+++ HWs/HW1/partB/AnagramSorter.java
@@ -9,47 +9,46 @@ import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
-	
+ 
 public class AnagramSorter {
-	
+ 
   public class Anagram { 
-  	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
-  		private final static IntWritable one = new IntWritable(1);
-  		private Text word = new Text();
-  		private Text sorted = new Text();
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+      private final static IntWritable one = new IntWritable(1);
+      private Text word = new Text();
+      private Text sorted = new Text();
 
-  	  public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-  	  	String line = value.toString();
-  	    StringTokenizer tokenizer = new StringTokenizer(line);
-  	    while (tokenizer.hasMoreTokens()) {
-  	    	word.set(tokenizer.nextToken());
-  				char[] chars = word.toString().toCharArray();
+      public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+          word.set(tokenizer.nextToken());
+          char[] chars = word.toString().toCharArray();
           Arrays.sort(chars);
-  				sorted.set(new String(chars));
-  	      output.collect(sorted, word);
-  	    }
-  	  }
-  	}
-  	
-  	public static class Reduce  extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
-  		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-      	//int sum = 0;
-      	//while (values.hasNext()) {
-      	//	sum += values.next().get();
-      	//}
-      	//output.collect(key, new IntWritable(sum));
-  			//ArrayList<IntWritable> list = new ArrayList<IntWritable>();    
+          sorted.set(new String(chars));
+          output.collect(sorted, word);
+        }
+      }
+    }
+   
+    public static class Reduce  extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+      public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        //int sum = 0;
+        //while (values.hasNext()) {
+        // sum += values.next().get();
+        //}
+        //output.collect(key, new IntWritable(sum));
+        //ArrayList<IntWritable> list = new ArrayList<IntWritable>();    
         StringBuilder strVal = new StringBuilder();
-      	while ( values.hasNext()) {
-           
-  				strVal.append(values.next().toString());
-  				strVal.append(",");
-      	}
-  			//output.collect(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()]) ) );
-  			output.collect(key, new Text(strVal.toString()));
-      	//context.write(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()])));
-    	}
-  	}
+        while ( values.hasNext()) {     
+          strVal.append(values.next().toString());
+          strVal.append(",");
+        }
+        //output.collect(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()]) ) );
+        output.collect(key, new Text(strVal.toString()));
+        //context.write(key, new MyArrayWritable(IntWritable.class, list.toArray(new IntWritable[list.size()])));
+      }
+    }
   }
 
   public class Sorter {
@@ -79,22 +78,22 @@ public class AnagramSorter {
   }
 
   public static void main(String[] args) throws Exception {
-  	// JobConf conf = new JobConf(AnagramSorter.class);
+    // JobConf conf = new JobConf(AnagramSorter.class);
     // conf.setJobName("AnagramSorter");
-		Configuration conf = getConf();
+    Configuration conf = getConf();
     Job job = new Job(conf, "secondary sort");
-		// set MAP output parameters
+    // set MAP output parameters
     job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-		// set RDC output paramters
+    job.setMapOutputValueClass(Text.class);
+    // set RDC output paramters
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
 
     job.setMapperClass(Map.class);
     //conf.setCombinerClass(Reduce.class);
     job.setReducerClass(Reduce.class);
-		
-		// set IO value
+  
+    // set IO value
     job.setInputFormat(TextInputFormat.class);
     job.setOutputFormat(TextOutputFormat.class);
 
@@ -103,35 +102,35 @@ public class AnagramSorter {
 
     // JobClient.runJob(conf);
     job.waitForCompletion(true);
-    
-  	/*
-   	* Sorting
-   	*/
-		// JobConf conf1 = new JobConf(AnagramSorter.class);
-  //   conf1.setJobName("AnagramSorter");
 
-  //   // set MAP output parameters
-  //   conf1.setMapOutputKeyClass(IntWritable.class);
-  //   conf1.setMapOutputValueClass(Text.class);
-  //   // set RDC output paramters
-  //   conf1.setOutputKeyClass(NullWritable.class);
-  //   conf1.setOutputValueClass(Text.class);
+    /*
+    * Sorting
+    */
+    // JobConf conf1 = new JobConf(AnagramSorter.class);
+    //   conf1.setJobName("AnagramSorter");
 
-  //   conf1.setMapperClass(Map_sort.class);
-  //   //conf.setCombinerClass(Reduce.class);
-  //   conf1.setReducerClass(Reduce_sort.class);
+    //   // set MAP output parameters
+    //   conf1.setMapOutputKeyClass(IntWritable.class);
+    //   conf1.setMapOutputValueClass(Text.class);
+    //   // set RDC output paramters
+    //   conf1.setOutputKeyClass(NullWritable.class);
+    //   conf1.setOutputValueClass(Text.class);
 
-  //   // set IO value
-  //   conf1.setInputFormat(TextInputFormat.class);
-  //   conf1.setOutputFormat(TextOutputFormat.class);
+    //   conf1.setMapperClass(Map_sort.class);
+    //   //conf.setCombinerClass(Reduce.class);
+    //   conf1.setReducerClass(Reduce_sort.class);
 
-  //   FileInputFormat.setInputPaths(conf, new Path("inter_dir/output/"));
-  //   FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    //   // set IO value
+    //   conf1.setInputFormat(TextInputFormat.class);
+    //   conf1.setOutputFormat(TextOutputFormat.class);
 
-  //   JobClient.runJob(conf);
+    //   FileInputFormat.setInputPaths(conf, new Path("inter_dir/output/"));
+    //   FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
-  	
-	}
+    //   JobClient.runJob(conf);
+
+   
+  }
 }
 
 
@@ -139,27 +138,27 @@ public class AnagramSorter {
 
 class MyArrayWritable extends ArrayWritable{
 
-	public MyArrayWritable(Class<? extends Writable> valueClass, Writable[] values) {
+ public MyArrayWritable(Class<? extends Writable> valueClass, Writable[] values) {
     super(valueClass, values);
-	}
-	public MyArrayWritable(Class<? extends Writable> valueClass) {
+ }
+ public MyArrayWritable(Class<? extends Writable> valueClass) {
     super(valueClass);
-	}
+ }
 
-	@Override
-	public IntWritable[] get() {
+ @Override
+ public IntWritable[] get() {
     return (IntWritable[]) super.get();
-	}
+ }
 
-	@Override
+ @Override
   public void write(DataOutput arg0) throws IOException {
-  	System.out.println("write method called");
-  	super.write(arg0);
-	}
+   System.out.println("write method called");
+   super.write(arg0);
+ }
   
   @Override
-	public String toString() {
-  	return Arrays.toString(get());
-	}
+ public String toString() {
+   return Arrays.toString(get());
+ }
 
 }
