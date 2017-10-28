5a72354285ada962508cdeb5a45ecacaee99602d
ehsan <qasemi.ehs@gmail.com>
2017-10-28 17:33:46 -0500

    timestamp type added

--- HWs/HW2/PartA/PartAQuestion1.py
+++ HWs/HW2/PartA/PartAQuestion1.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 from pyspark.sql.functions import window
-from pyspark.sql.types import StructType
+from pyspark.sql.types import StructType, TimestampType
 
 spark = SparkSession \
 	.builder \
@@ -21,22 +21,19 @@ spark = SparkSession \
 
 # Read all the csv files written atomically in a directory
 # userA, userB, timestamp, interaction
-
-# userSchema = StructType()\
-# 	.add("userA", "integer")\
-# 	.add("userB", "integer")\
-# 	.add("timestamp", "Timestamp")\
-# 	.add("interaction","string")
+userSchema = StructType()\
+	.add("userA", "integer")\
+	.add("userB", "integer")\
+	.add("timestamp", TimestampType())\
+	.add("interaction","string")
 
 activity = spark \
 	.readStream \
 	.option("sep", ",") \
-	.option("inferSchema", "true")\
-	.csv("higgs/stage")  
-	# Equivalent to format("csv").load("/path/to/directory")
-	#.schema(userSchema) \
-	
+	.schema(userSchema) \
+	.csv("higgs/stage")  # Equivalent to format("csv").load("/path/to/directory")
 
+#.option("inferSchema", "true")
 
 # Split the lines into words
 # words = lines.select(
