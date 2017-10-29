4eac314ffd8e12b0d0901e7cffac8ff0c903739c
ehsan <qasemi.ehs@gmail.com>
2017-10-29 15:26:00 -0500

    changing the output format to parquet

--- HWs/HW2/PartA/PartAQuestion2.py
+++ HWs/HW2/PartA/PartAQuestion2.py
@@ -53,7 +53,7 @@ wordCounts = activity.groupBy(
 # Start running the query that prints the running counts to the console
 query = wordCounts \
 	.writeStream \
-	.outputMode("complete") \
+	.outputMode("append") \
 	.format("parquet") \
 	.option("path", "higgs/stage") \
 	.start()
