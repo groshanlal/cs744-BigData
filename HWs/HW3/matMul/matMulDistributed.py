7871a8a3c65e049ed317a9330111d4c8bd96b75e
ehsan <qasemi.ehs@gmail.com>
2017-11-22 15:41:44 -0600

    the N=100 case works, testing the N=3000 d=3 case

--- HWs/HW3/matMul/matMulDistributed.py
+++ HWs/HW3/matMul/matMulDistributed.py
@@ -12,7 +12,7 @@ import os
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
 N = 1000 # dimension of the matrix
-d = 2 # number of splits along one dimension. Thus, we will have 100 blocks
+d = 3 # number of splits along one dimension. Thus, we will have 100 blocks
 M = int(N / d)
 
 n_m = 5;
