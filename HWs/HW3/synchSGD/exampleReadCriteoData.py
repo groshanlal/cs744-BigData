3c09b79b2e6b9b2761932af30922d7f8ca2f877f
ehsan <qasemi.ehs@gmail.com>
2017-11-25 18:28:03 -0600

    add batching and the new griadient calculation method to the SGD code

--- HWs/HW3/synchSGD/exampleReadCriteoData.py
+++ HWs/HW3/synchSGD/exampleReadCriteoData.py
@@ -71,7 +71,7 @@ with g.as_default():
     def calc_gradient(X,W,Y):
         error = tf.sigmoid(tf.mul(Y,tf.matmul(X,W)))
         print error.get_shape()
-        error_m1 = tf.subtract(error,1)
+        error_m1 = error-1
         print error_m1.get_shape()
         gradient = tf.mul(Y,tf.matmul(X,error_m1))
         print gradient.get_shape()
