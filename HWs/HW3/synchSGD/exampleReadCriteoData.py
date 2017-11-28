864555264dfee13898595fefa131477b333ef97f
ehsan <qasemi.ehs@gmail.com>
2017-11-27 20:35:10 -0600

    reseting the reader test code to work on the sparse to dense function

--- HWs/HW3/synchSGD/exampleReadCriteoData.py
+++ HWs/HW3/synchSGD/exampleReadCriteoData.py
@@ -2,101 +2,65 @@ import tensorflow as tf
 
 # number of features in the criteo dataset after one-hot encoding
 num_features = 33762578
-s_batch = 40
+
 
 # Here, we will show how to include reader operators in the TensorFlow graph.
 # These operators take as input list of filenames from which they read data.
 # On every invocation of the operator, some records are read and passed to the
 # downstream vertices as Tensors
 
-file_dict = {0:["00","01","02","03","04"],
-             1:["05","06","07","08","09"],
-             2:["10","11","12","13","14"],
-             3:["15","16","17","18","19"],
-             4:["20","21"],
-            -1:["22"]}
-
 
 g = tf.Graph()
 
 with g.as_default():
 
-    def get_datapoint_iter(file_idx=[]):
-        fileNames = map(lambda s: "/home/ubuntu/criteo-tfr-tiny/tfrecords"+s,file_idx)
-        # We first define a filename queue comprising 5 files.
-        filename_queue = tf.train.string_input_producer(fileNames, num_epochs=None)
-
-         # TFRecordReader creates an operator in the graph that reads data from queue
-        reader = tf.TFRecordReader()
-
-        # Include a read operator with the filenae queue to use. The output is a string
-        # Tensor called serialized_example
-        _, serialized_example = reader.read_up_to(filename_queue,s_batch)
+    # We first define a filename queue comprising 5 files.
+    filename_queue = tf.train.string_input_producer([
+        "./data/criteo-tfr-tiny/tfrecords00",
+        "./data/criteo-tfr-tiny/tfrecords01",
+        "./data/criteo-tfr-tiny/tfrecords02",
+        "./data/criteo-tfr-tiny/tfrecords03",
+        "./data/criteo-tfr-tiny/tfrecords04",
+    ], num_epochs=None)
 
 
-        # The string tensors is essentially a Protobuf serialized string. With the
-        # following fields: label, index, value. We provide the protobuf fields we are
-        # interested in to parse the data. Note, feature here is a dict of tensors
-        features = tf.parse_example(serialized_example,
-                                           features={
-                                            'label': tf.FixedLenFeature([1], dtype=tf.int64),
-                                            'index' : tf.VarLenFeature(dtype=tf.int64),
-                                            'value' : tf.VarLenFeature(dtype=tf.float32),
-                                           }
-                                          )
+    # TFRecordReader creates an operator in the graph that reads data from queue
+    reader = tf.TFRecordReader()
 
-        label = features['label']
-        index = features['index']
-        value = features['value']
-
-        # since we parsed a VarLenFeatures, they are returned as SparseTensors.
-        # To run operations on then, we first convert them to dense Tensors as below.
-        dense_feature = tf.sparse_to_dense(tf.sparse_tensor_to_dense(index),
-                                       [num_features,],
-        #                               tf.constant([33762578, 1], dtype=tf.int64),
-                                       tf.sparse_tensor_to_dense(value))
-        return (dense_feature,tf.cast(label, tf.float32))
+    # Include a read operator with the filenae queue to use. The output is a string
+    # Tensor called serialized_example
+    _, serialized_example = reader.read(filename_queue)
 
 
-    def next_batch(id = 0):
-        X = []
-        Y = []
-        for i in range(s_batch):
-            x_,y_ = get_datapoint_iter(file_dict[id])
-            X.append(x_)
-            Y.append(y_)
-        return tf.pack(X),tf.pack(Y)
-   
+    # The string tensors is essentially a Protobuf serialized string. With the
+    # following fields: label, index, value. We provide the protobuf fields we are
+    # interested in to parse the data. Note, feature here is a dict of tensors
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                        'label': tf.FixedLenFeature([1], dtype=tf.int64),
+                                        'index' : tf.VarLenFeature(dtype=tf.int64),
+                                        'value' : tf.VarLenFeature(dtype=tf.float32),
+                                       }
+                                      )
 
-    # x_,y_ = get_datapoint_iter(file_dict[id])
-        # min_after_dequeue = 100
-        # capacity = min_after_dequeue + 3 * s_batch
-        # example_batch, label_batch = tf.train.shuffle_batch(
-        #         [x_, y_], batch_size=s_batch, capacity=capacity,
-        #         min_after_dequeue=min_after_dequeue)
+    label = features['label']
+    index = features['index']
+    value = features['value']
 
-        # return example_batch, label_batch
-    
-    def calc_gradient(X,W,Y):
-        error = tf.sigmoid(tf.mul(Y,tf.matmul(X,W)))
-        print "error:",error.get_shape()
-        error_m1 = error-1
-        print "error_m1:",error_m1.get_shape()
-        gradient = tf.matmul(tf.transpose(X),tf.mul(Y,error_m1))
-        print "gradient:",gradient.get_shape()
-        return tf.reduce_sum(gradient,1)
+    # These print statements are there for you see the type of the following
+    # variables
+    print label
+    print index
+    print value
 
-    w = tf.Variable(tf.zeros([num_features, 1]), name="model")
+    # since we parsed a VarLenFeatures, they are returned as SparseTensors.
+    # To run operations on then, we first convert them to dense Tensors as below.
+    dense_feature = tf.sparse_to_dense(tf.sparse_tensor_to_dense(index),
+                                   [num_features,],
+    #                               tf.constant([33762578, 1], dtype=tf.int64),
+                                   tf.sparse_tensor_to_dense(value))
 
 
-    dense_feature,label =  get_datapoint_iter(file_dict[0]) #next_batch()
-
-    print "dense_feature:",dense_feature.get_shape()
-    print "label:",label.get_shape()
-
-    grad = calc_gradient(dense_feature,w,label)
-
-   
     # as usual we create a session.
     sess = tf.Session()
     sess.run(tf.initialize_all_variables())
@@ -105,11 +69,8 @@ with g.as_default():
     # Effectively, it spins up separate threads to read from the files
     tf.train.start_queue_runners(sess=sess)
 
-    for i in range(0, 1):
+    for i in range(0, 20):
         # every time we call run, a new data point is read from the files
-        gradient,X,Y =  sess.run([grad,dense_feature,label])
-        print X.shape
-        print Y.shape
-        print gradient.shape
-        print Y
-        # print sum(output)
+        output =  sess.run(dense_feature)
+        print output.shape
+        print sum(output)
---
+++ HWs/HW3/synchSGD/exampleReadCriteoData_not_working.py
@@ -0,0 +1,115 @@
+import tensorflow as tf
+
+# number of features in the criteo dataset after one-hot encoding
+num_features = 33762578
+s_batch = 40
+
+# Here, we will show how to include reader operators in the TensorFlow graph.
+# These operators take as input list of filenames from which they read data.
+# On every invocation of the operator, some records are read and passed to the
+# downstream vertices as Tensors
+
+file_dict = {0:["00","01","02","03","04"],
+             1:["05","06","07","08","09"],
+             2:["10","11","12","13","14"],
+             3:["15","16","17","18","19"],
+             4:["20","21"],
+            -1:["22"]}
+
+
+g = tf.Graph()
+
+with g.as_default():
+
+    def get_datapoint_iter(file_idx=[]):
+        fileNames = map(lambda s: "/home/ubuntu/criteo-tfr-tiny/tfrecords"+s,file_idx)
+        # We first define a filename queue comprising 5 files.
+        filename_queue = tf.train.string_input_producer(fileNames, num_epochs=None)
+
+         # TFRecordReader creates an operator in the graph that reads data from queue
+        reader = tf.TFRecordReader()
+
+        # Include a read operator with the filenae queue to use. The output is a string
+        # Tensor called serialized_example
+        _, serialized_example = reader.read_up_to(filename_queue,s_batch)
+
+
+        # The string tensors is essentially a Protobuf serialized string. With the
+        # following fields: label, index, value. We provide the protobuf fields we are
+        # interested in to parse the data. Note, feature here is a dict of tensors
+        features = tf.parse_example(serialized_example,
+                                           features={
+                                            'label': tf.FixedLenFeature([1], dtype=tf.int64),
+                                            'index' : tf.VarLenFeature(dtype=tf.int64),
+                                            'value' : tf.VarLenFeature(dtype=tf.float32),
+                                           }
+                                          )
+
+        label = features['label']
+        index = features['index']
+        value = features['value']
+
+        # since we parsed a VarLenFeatures, they are returned as SparseTensors.
+        # To run operations on then, we first convert them to dense Tensors as below.
+        dense_feature = tf.sparse_to_dense(tf.sparse_tensor_to_dense(index),
+                                       [num_features,],
+        #                               tf.constant([33762578, 1], dtype=tf.int64),
+                                       tf.sparse_tensor_to_dense(value))
+        return (dense_feature,tf.cast(label, tf.float32))
+
+
+    def next_batch(id = 0):
+        X = []
+        Y = []
+        for i in range(s_batch):
+            x_,y_ = get_datapoint_iter(file_dict[id])
+            X.append(x_)
+            Y.append(y_)
+        return tf.pack(X),tf.pack(Y)
+   
+
+    # x_,y_ = get_datapoint_iter(file_dict[id])
+        # min_after_dequeue = 100
+        # capacity = min_after_dequeue + 3 * s_batch
+        # example_batch, label_batch = tf.train.shuffle_batch(
+        #         [x_, y_], batch_size=s_batch, capacity=capacity,
+        #         min_after_dequeue=min_after_dequeue)
+
+        # return example_batch, label_batch
+    
+    def calc_gradient(X,W,Y):
+        error = tf.sigmoid(tf.mul(Y,tf.matmul(X,W)))
+        print "error:",error.get_shape()
+        error_m1 = error-1
+        print "error_m1:",error_m1.get_shape()
+        gradient = tf.matmul(tf.transpose(X),tf.mul(Y,error_m1))
+        print "gradient:",gradient.get_shape()
+        return tf.reduce_sum(gradient,1)
+
+    w = tf.Variable(tf.zeros([num_features, 1]), name="model")
+
+
+    dense_feature,label =  get_datapoint_iter(file_dict[0]) #next_batch()
+
+    print "dense_feature:",dense_feature.get_shape()
+    print "label:",label.get_shape()
+
+    grad = calc_gradient(dense_feature,w,label)
+
+   
+    # as usual we create a session.
+    sess = tf.Session()
+    sess.run(tf.initialize_all_variables())
+
+    # this is new command and is used to initialize the queue based readers.
+    # Effectively, it spins up separate threads to read from the files
+    tf.train.start_queue_runners(sess=sess)
+
+    for i in range(0, 1):
+        # every time we call run, a new data point is read from the files
+        gradient,X,Y =  sess.run([grad,dense_feature,label])
+        print X.shape
+        print Y.shape
+        print gradient.shape
+        print Y
+        # print sum(output)
