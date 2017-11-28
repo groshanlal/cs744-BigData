3ce4cb85f1a0f49d194ff64163c9a7cf5b00412d
ehsan <qasemi.ehs@gmail.com>
2017-11-27 22:55:54 -0600

    reseting the synchronous code to make sure it works on the cluster

--- HWs/HW3/synchSGD/exampleReadCriteoData.py
+++ HWs/HW3/synchSGD/exampleReadCriteoData.py
@@ -23,65 +23,68 @@ g = tf.Graph()
 
 with g.as_default():
 
-    # We first define a filename queue comprising 5 files.
-    filename_queue = tf.train.string_input_producer([
-        "/home/ubuntu/criteo-tfr-tiny/tfrecords00",
-        "/home/ubuntu/criteo-tfr-tiny/tfrecords01",
-        "/home/ubuntu/criteo-tfr-tiny/tfrecords02",
-        "/home/ubuntu/criteo-tfr-tiny/tfrecords03",
-        "/home/ubuntu/criteo-tfr-tiny/tfrecords04",
-    ], num_epochs=None)
+    def get_datapoint_iter():
+        # We first define a filename queue comprising 5 files.
+        filename_queue = tf.train.string_input_producer([
+            "/home/ubuntu/criteo-tfr-tiny/tfrecords00",
+            "/home/ubuntu/criteo-tfr-tiny/tfrecords01",
+            "/home/ubuntu/criteo-tfr-tiny/tfrecords02",
+            "/home/ubuntu/criteo-tfr-tiny/tfrecords03",
+            "/home/ubuntu/criteo-tfr-tiny/tfrecords04",
+        ], num_epochs=None)
 
 
-    # TFRecordReader creates an operator in the graph that reads data from queue
-    reader = tf.TFRecordReader()
+        # TFRecordReader creates an operator in the graph that reads data from queue
+        reader = tf.TFRecordReader()
 
-    # Include a read operator with the filenae queue to use. The output is a string
-    # Tensor called serialized_example
-    _, serialized_example = reader.read(filename_queue)
-
-
-    # The string tensors is essentially a Protobuf serialized string. With the
-    # following fields: label, index, value. We provide the protobuf fields we are
-    # interested in to parse the data. Note, feature here is a dict of tensors
-    features = tf.parse_single_example(serialized_example,
-                                       features={
-                                        'label': tf.FixedLenFeature([1], dtype=tf.int64),
-                                        'index' : tf.VarLenFeature(dtype=tf.int64),
-                                        'value' : tf.VarLenFeature(dtype=tf.float32),
-                                       }
-                                      )
-
-    label = features['label']
-    index = features['index']
-    value = features['value']
-
-    # These print statements are there for you see the type of the following
-    # variables
-    print label
-    print index
-    print value
-
-    # since we parsed a VarLenFeatures, they are returned as SparseTensors.
-    # To run operations on then, we first convert them to dense Tensors as below.
-    dense_feature = tf.sparse_to_dense(tf.sparse_tensor_to_dense(index),
-                                   [33762578,],
-    #                               tf.constant([33762578, 1], dtype=tf.int64),
-                                   tf.sparse_tensor_to_dense(value))
+        # Include a read operator with the filenae queue to use. The output is a string
+        # Tensor called serialized_example
+        _, serialized_example = reader.read(filename_queue)
 
 
-    label_flt = tf.cast(label, tf.float32)
-    # min_after_dequeue defines how big a buffer we will randomly sample
-    #   from -- bigger means better shuffling but slower start up and more
-    #   memory used.
-    # capacity must be larger than min_after_dequeue and the amount larger
-    #   determines the maximum we will prefetch.  Recommendation:
-    #   min_after_dequeue + (num_threads + a small safety margin) * batch_size
-    min_after_dequeue = 10
-    capacity = min_after_dequeue + 3 * s_batch
-    example_batch, label_batch = tf.train.shuffle_batch(
-      [dense_feature[0:num_features], label_flt], batch_size=s_batch, capacity=capacity,
-      min_after_dequeue=min_after_dequeue)
+        # The string tensors is essentially a Protobuf serialized string. With the
+        # following fields: label, index, value. We provide the protobuf fields we are
+        # interested in to parse the data. Note, feature here is a dict of tensors
+        features = tf.parse_single_example(serialized_example,
+                                           features={
+                                            'label': tf.FixedLenFeature([1], dtype=tf.int64),
+                                            'index' : tf.VarLenFeature(dtype=tf.int64),
+                                            'value' : tf.VarLenFeature(dtype=tf.float32),
+                                           }
+                                          )
+
+        label = features['label']
+        index = features['index']
+        value = features['value']
+
+        # These print statements are there for you see the type of the following
+        # variables
+        print label
+        print index
+        print value
+
+        # since we parsed a VarLenFeatures, they are returned as SparseTensors.
+        # To run operations on then, we first convert them to dense Tensors as below.
+        dense_feature = tf.sparse_to_dense(tf.sparse_tensor_to_dense(index),
+                                       [33762578,],
+        #                               tf.constant([33762578, 1], dtype=tf.int64),
+                                       tf.sparse_tensor_to_dense(value))
+
+
+        label_flt = tf.cast(label, tf.float32)
+        # min_after_dequeue defines how big a buffer we will randomly sample
+        #   from -- bigger means better shuffling but slower start up and more
+        #   memory used.
+        # capacity must be larger than min_after_dequeue and the amount larger
+        #   determines the maximum we will prefetch.  Recommendation:
+        #   min_after_dequeue + (num_threads + a small safety margin) * batch_size
+        min_after_dequeue = 10
+        capacity = min_after_dequeue + 3 * s_batch
+        example_batch, label_batch = tf.train.shuffle_batch(
+          [dense_feature[0:num_features], label_flt], batch_size=s_batch, capacity=capacity,
+          min_after_dequeue=min_after_dequeue)
+
+        return example_batch, label_batch
 
 
     def calc_gradient(X,W,Y):
@@ -93,9 +96,12 @@ with g.as_default():
         print "gradient:",gradient.get_shape()
         return tf.reduce_sum(gradient,1)
 
+
+
+
     w = tf.Variable(tf.ones([num_features, 1]), name="model")
 
-
+    example_batch, label_batch = get_datapoint_iter()
     grad = calc_gradient(example_batch,w,label_batch)
 
 
