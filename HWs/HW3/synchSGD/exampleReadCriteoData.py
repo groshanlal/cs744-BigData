22e6009a010d0720b76f66b76152c9dcfa2b54fd
ehsan <qasemi.ehs@gmail.com>
2017-11-27 20:58:21 -0600

    reseting the reader test code to work on the sparse to dense function

--- HWs/HW3/synchSGD/exampleReadCriteoData.py
+++ HWs/HW3/synchSGD/exampleReadCriteoData.py
@@ -69,9 +69,9 @@ with g.as_default():
     #   determines the maximum we will prefetch.  Recommendation:
     #   min_after_dequeue + (num_threads + a small safety margin) * batch_size
     min_after_dequeue = 10000
-    capacity = min_after_dequeue + 3 * batch_size
+    capacity = min_after_dequeue + 3 * s_batch
     example_batch, label_batch = tf.train.shuffle_batch(
-      [dense_feature, label], batch_size=batch_size, capacity=capacity,
+      [dense_feature, label], batch_size=s_batch, capacity=capacity,
       min_after_dequeue=min_after_dequeue)
 
 
