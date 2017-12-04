a63ddb6331d30e98e637cb497731acc77ad879cd
ehsan <qasemi.ehs@gmail.com>
2017-12-03 22:55:26 -0600

    getting rid of other tasks for testing

--- HWs/HW3/asynchSGD/exampleAsync.sh
+++ HWs/HW3/asynchSGD/exampleAsync.sh
@@ -9,7 +9,7 @@ start_cluster startserver.py
 echo "running main task"
 nohup python exampleAsynchronousUpdate.py --task_index=0 &
 # nohup python exampleAsynchronousUpdate.py --task_index=0 > asynclog-0.out 2>&1&
-sleep 4 # wait for variable to be initialized
+sleep 10 # wait for variable to be initialized
 echo "running other tasks"
 nohup python exampleAsynchronousUpdate.py --task_index=1 > asynclog-1.out 2>&1&
 # nohup python exampleAsynchronousUpdate.py --task_index=2 > asynclog-2.out 2>&1&
