9a367914fb0b005b4f5a669dca2ca0b7d82a3ed5
ehsan <qasemi.ehs@gmail.com>
2017-12-04 01:33:12 -0600

    updating th asynch

--- HWs/HW3/asynchSGD/exampleAsync.sh
+++ HWs/HW3/asynchSGD/exampleAsync.sh
@@ -12,7 +12,7 @@ nohup python exampleAsynchronousUpdate.py --task_index=0 > asynclog-0.out 2>&1&
 sleep 10 # wait for variable to be initialized
 echo "running other tasks"
 nohup python exampleAsynchronousUpdate.py --task_index=1 > asynclog-1.out 2>&1&
-# nohup python exampleAsynchronousUpdate.py --task_index=2 > asynclog-2.out 2>&1&
-# nohup python exampleAsynchronousUpdate.py --task_index=3 > asynclog-3.out 2>&1&
-# nohup python exampleAsynchronousUpdate.py --task_index=4 > asynclog-4.out 2>&1&
+nohup python exampleAsynchronousUpdate.py --task_index=2 > asynclog-2.out 2>&1&
+nohup python exampleAsynchronousUpdate.py --task_index=3 > asynclog-3.out 2>&1&
+nohup python exampleAsynchronousUpdate.py --task_index=4 > asynclog-4.out 2>&1&
 echo "all tasks are funcitonal"
